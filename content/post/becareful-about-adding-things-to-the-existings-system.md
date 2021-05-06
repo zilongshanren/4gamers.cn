afd583f5930404e93a96a8f5be6718ce7c0b03db
zilongshanren <guanghui8827@126.com>
2021-05-06 11:33:48 +0800

    archive 4gamers.cn

--- content/post/becareful-about-adding-things-to-the-existings-system.md
+++
@@ -1,6 +0,0 @@
-+++
-title = "be careful about adding things to the existings system"
-lastmod = 2021-02-25T23:48:29+08:00
-draft = true
-author = "zilongshanren"
-+++
--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,2 @@
-hugo --theme=even --baseUrl="https://zilongshanren.com"
-hugo && rsync -avz --delete public/ root@112.124.58.18:/var/www/octopress-cn
+hugo --theme=even --baseUrl="https://4gamers.cn"
+hugo && rsync -avz --delete public/ root@112.124.58.18:/var/www/4gamers.cn
